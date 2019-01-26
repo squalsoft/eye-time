ccfe81eff597c126cd7bf9171b8e3720a7172c1c
mr_squall <squalsoft@gmail.com>
2019-01-26 17:23:19 +0300

    build config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? `${process.cwd()}/dist/`
+      : publicPath, // '/' by default
   configureWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
-      config.output.publicPath = `${process.cwd()}/dist/`;
-    }
-    config.target = "electron-renderer";
+    // if (process.env.NODE_ENV === 'production') {
+    //   config.output.publicPath = ;
+    // }
+    config.target = 'electron-renderer';
   }
 };
